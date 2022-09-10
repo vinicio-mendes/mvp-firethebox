a8600e317ca09a5bcc301f6c6dfb1f9861ab500e
Vinicio Mendes <contatoviniciom@gmail.com>
2022-09-09 22:16:56 -0300

    update layout

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,51 @@
-export function Lesson(){
+import { CheckCircle, Lock } from 'phosphor-react'
+import { format, isPast  } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+// "EEEE' • 'd' de 'MMMM' • 'k'h'mm",
+
+export function Lesson(props: LessonProps){
+    const isLessonAvailable = isPast(props.availableAt)
+    const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+        locale: ptBR,
+    })
+
     return (
-        <h1>Lesson</h1>
+        <a href="#">
+            <span className="text-gray-300">
+                {availableDateFormatted}
+                {/* Segunda • 05 de Setembro • 19h00 */}
+            </span>
+
+            <div className="rounded border border-gray-500 p-4 mt-2">
+                <header className="flex items-center justify-between">
+                    {isLessonAvailable ? (
+                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                            <CheckCircle size={20} />
+                            Conteúdo liberado
+                        </span>
+                    ) : (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                            <Lock size={20} />
+                            Em breve
+                        </span>
+                    )}
+                    <span className="text-xs rounded py-[0.25rem] px-2 text-white border border-green-300 font-bold">
+                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+                    </span>
+                </header>
+
+                <strong className="text-gray-200 mt-5 block">
+                    Abertura do evento StarTrail
+                </strong>
+            </div>
+        </a>
     )
 }
